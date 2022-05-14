f8154e5393dd8f36f293b8d200c1630e64bed5dd
Ritik Sahu <ritikrks@gmail.com>
2022-05-14 15:21:28 +0530

    added filters

--- Scraper.py
+++ Scraper.py
@@ -6,11 +6,38 @@ import config
 def filter_jobs(job_data, keyword, projectLength, unspecifiedJobs, hourlyRateMin, hourlyRateMax, paymentVerified, paymentUnverified, jobType, countries, create_file=True):
     if len(job_data) == 0:
         return 404
-    res_data = []
-    # TODO: filter the conditions here
+
+    file_header = job_data[0].keys()
+    job_data_length = len(job_data)
+    projectLength_map = {1: projectLength['zero'], 2: projectLength['short'], 3: projectLength['medium'], 4: projectLength['long']}
+    jobType_map = {1: jobType['entry'], 2: jobType['intermediate'], 3: jobType['expert']}
+    
+    i = 0
+    count = 0
+    while count < job_data_length:
+        count += 1
+        if (
+            (projectLength_map[job_data[i]['project_length']])  and
+            (not job_data[i]['is_job_fixed'] and 
+                (
+                    (job_data[i]['hourly_budget_min'] and job_data[i]['hourly_budget_max']) and
+                    (hourlyRateMin<=job_data[i]['hourly_budget_min'] and hourlyRateMax<=job_data[i]['hourly_budget_max'])
+                )
+                or (unspecifiedJobs and not (job_data[i]['hourly_budget_min'] and job_data[i]['hourly_budget_max']))
+            ) and
+            (jobType_map[job_data[i]['job_level']]) and
+            (not job_data[i]['country'] in countries) and
+            (not(paymentVerified ^ paymentUnverified) or ((paymentVerified and job_data[i]['is_payment_verified']) or (paymentUnverified and not job_data[i]['is_payment_verified'])))
+        ):
+            i += 1
+            continue
+
+        # remove that job from job_data
+        del job_data[i] 
+
     if create_file:
         with open(os.path.join("static",f'data/{keyword}.csv'), 'w', newline='', encoding="utf-8") as csvfile:
-            csv_writer = csv.DictWriter(csvfile, job_data[0].keys())
+            csv_writer = csv.DictWriter(csvfile, file_header)
             csv_writer.writeheader()
             csv_writer.writerows(job_data)
     return 200
